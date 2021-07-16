1340cf51c0bb6cd4b1e91f8fb9b8a652860bee32
naka-no-mura <nakanomura.ryu@gmail.com>
2021-07-16 10:41:07 +0900

    add:style.css

--- public/items/item.php
+++ public/items/item.php
@@ -15,6 +15,9 @@ if ($_GET['flower_id']) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;800;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="../../app/assets/stylesheets/style.css" type="text/css">
   <title>ハナエラビ</title>
 </head>
--- public/meaning/meaning.php
+++ public/meaning/meaning.php
@@ -15,23 +15,26 @@ if ($_GET['season']) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;800;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="../../app/assets/stylesheets/style.css" type="text/css">
   <title>ハナエラビ</title>
 </head>
 <body>
   <head>
     <h1>今の気分は？</h1>
+  </head>
+  <main>
     <p><?php echo print(htmlspecialchars($_GET['season'], ENT_QUOTES)); ?></p>
     <div>
       <?php foreach ($fmeanings as $fmeaning): ?>
-        <form action="../items/item.php" method="get">
+        <form action="../items/item.php" method="get" class="meanings">
           <input type="hidden" name="flower_id" value="<?php echo $fmeaning['id'] ?>">
-          <input type="submit" value="<?php echo htmlspecialchars($fmeaning['meaning'], ENT_QUOTES); ?>">
+          <input type="submit" class="meaning" value="<?php echo htmlspecialchars($fmeaning['meaning'], ENT_QUOTES); ?>">
         </form>
       <?php endforeach; ?>
     </div>
-  </head>
-  <main>
   </main>
 </body>
 </html>
