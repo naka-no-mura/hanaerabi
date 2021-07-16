25e642338755e9c022b3371289d155e7a9717caa
naka-no-mura <nakanomura.ryu@gmail.com>
2021-07-16 17:34:11 +0900

    fix:DB接続項目

--- app/config/config.php
+++ app/config/config.php
@@ -5,8 +5,4 @@ require_once(__DIR__ . '/../classes/Flowers.php');
 require_once(__DIR__ . '/../classes/FlowerLanguages.php');
 require_once(__DIR__ . '/../functions/dbconnect.php');
 
-define('DSN', 'mysql:dbname=hanaerabi;host=localhost;charset=utf8');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-
 ?>
--- app/functions/dbconnect.php
+++ app/functions/dbconnect.php
@@ -1,21 +1,43 @@
 <?php
 
+// 本番環境用
 function getPdoInstance($pdo) {
   try {
-    $pdo = new PDO(
-      DSN,
-      DB_USER,
-      DB_PASS,
-      [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-      ]
-    );
-
+    $pdo = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+    $pdo['dbname'] = ltrim($pdo['path'], '/');
+    $dsn = "mysql:host={$pdo['host']};dbname={$pdo['dbname']};charset=utf8";
+    $user = $pdo['user'];
+    $pass = $pdo['pass'];
+    $options = [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+    ];
+    $pdo = new PDO($dsn, $user, $pass, $options);
     return $pdo;
   } catch (PDOException $e) {
     echo $e->getMessage();
     exit();
   }
 }
+
+// 開発環境用
+// function getPdoInstance($pdo) {
+//   try {
+//     $dsn = "mysql:dbname=hanaerabi;host=localhost;charset=utf8";
+//     $user = 'root';
+//     $pass = 'root';
+//     $options = [
+//       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//       PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+//     ];
+//     $pdo = new PDO($dsn, $user, $pass, $options);
+//     return $pdo;
+//   } catch (PDOException $e) {
+//     echo $e->getMessage();
+//     exit();
+//   }
+// }
 
 ?>
